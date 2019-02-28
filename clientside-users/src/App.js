d9e1b9fda4411fd78b0ed3bc470706074f5ae0ba
croy <roy.christie95@gmail.com>
2019-02-28 10:42:44 -0600

    Added homepage

--- clientside-users/src/App.js
+++ clientside-users/src/App.js
@@ -6,20 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>The Mines of Moria</h1>
+        <p>The legendary hospitality of the dwarves would welcome you, but for the evil that has overtaken this land.</p>
+        <p>It has been too long since you dwelt in these halls. You no longer know what to find here.</p>
+        <p>Proceed with caution, proceed with care.</p>
       </div>
     );
   }
